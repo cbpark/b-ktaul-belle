25327f11b7a88d582aa906f1752c65ea364f528e
cbpark <cbpark@gmail.com>
2022-04-02 00:03:56 +0900

    Add mRecoil function

--- src/constant.h
+++ src/constant.h
@@ -3,9 +3,11 @@
 
 #include <Math/Boost.h>
 #include <Math/Vector4D.h>
+#include <cmath>
 
 namespace analysis {
 constexpr double MB = 5.28;  // 5.279;
+constexpr double MBSQ = MB * MB;
 
 constexpr double MK = 0.4937;
 
@@ -16,6 +18,12 @@ constexpr double MPI = 0.13957;
 constexpr double MD = 1.864;
 
 constexpr double SQRTS = 10.583;
+
+/// the energy of B meson at CM frame.
+constexpr double EBCM = 0.5 * SQRTS;
+constexpr double PBCMSQ = EBCM * EBCM - MBSQ;
+/// |p| of B meson at CM frame.
+inline double PBCM() { return std::sqrt(PBCMSQ); }
 
 constexpr double EEM = 8.0;
 constexpr double EEP = 3.5;
--- src/variable.cc
+++ src/variable.cc
@@ -9,22 +9,25 @@
 #include "input.h"
 
 namespace analysis {
+double mRecoil(const Input &input, double cos_theta) {
+    auto kl_sig = input.k_sig() + input.mu_sig();
+    double m_kl_sq = kl_sig.mass2();
+    double e_kl = kl_sig.e();
+    double p_kl = kl_sig.P();
+
+    double m_tau_sq =
+        MBSQ + m_kl_sq - 2.0 * (EBCM * e_kl + PBCM() * p_kl * cos_theta);
+    return m_tau_sq < 0.0 ? -1.0 : std::sqrt(m_tau_sq);
+}
+
 M2Reconstruction mkM2Reconstruction(
     const Input &input, const std::optional<yam2::M2Solution> &m2sol) {
     if (!m2sol) { return {-1.0, -1.0}; }
 
     auto m2sol_ = m2sol.value();
-    auto k2sol = m2sol_.k2();
-    // reconstructed invisible momentum on the tag side.
-    LorentzVector k2sol_{k2sol.px(), k2sol.py(), k2sol.pz(), k2sol.e()};
-
-    // reconstructed B_tag using the M2 solution.
-    auto p_b_tag = input.vis_tag() + k2sol_;
-
-    // p(CM) = p(B_sig) + p(B_tag)
-    //       = p(K_sig l_sig) + p(tau_sig) + p(B_tag).
-    // therefore, p(tau_sig) = p(CM) - p(K_sig l_sig) - p(B_tag).
-    auto p_tau = CM() - input.kl_sig() - p_b_tag;
+    auto k1sol = m2sol_.k1();
+    LorentzVector k1sol_{k1sol.px(), k1sol.py(), k1sol.pz(), k1sol.e()};
+    auto p_tau = input.htau_sig() + k1sol_;
     double mtau_sq = p_tau.mass2();
 
     // why negative?
@@ -34,19 +37,7 @@ M2Reconstruction mkM2Reconstruction(
 }
 
 double mRecoilRandom(const Input &input, std::shared_ptr<TRandom3> rnd) {
-    auto kl_sig = input.k_sig() + input.mu_sig();
-    double m_kl_sq = kl_sig.mass2();
-    double e_kl = kl_sig.e();
-    double p_kl = kl_sig.P();
-
-    double m_b_sq = MB * MB;
-    double e_b = 0.5 * SQRTS;
-    double p_b = std::sqrt(e_b * e_b - m_b_sq);
-
     double cos_theta = rnd->Uniform(-1.0, 1.0);
-
-    double m_tau_sq =
-        m_b_sq + m_kl_sq - 2.0 * (e_b * e_kl + p_b * p_kl * cos_theta);
-    return m_tau_sq < 0.0 ? -1.0 : std::sqrt(m_tau_sq);
+    return mRecoil(input, cos_theta);
 }
 }  // namespace analysis
